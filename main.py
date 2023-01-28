6f90e18b7f664f54eac6c8c2a6373a4dd8e053f6
parisa-rahmanipour <parisrahmani93@gmail.com>
2023-01-28 16:50:28 +0330

    style(shopping_list_fuctional): obey flake8 rules

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import os
 import json
 import random
 import logging
-from os import path
 import logging.config
 from helper import msg
 from datetime import datetime
 from helper.msg import DISCOUNT_CODES
 from typing import (List,
-Tuple,
-Dict
-)
+                    Tuple,
+                    Dict)
 
 
-logging.config.fileConfig(fname='log/handler.toml', disable_existing_loggers=False)
-logger=logging.getLogger("log/authLogger")
+logging.config.fileConfig(fname='log/handler.toml', disable_existing_loggers=False) # noqa E501
+logger = logging.getLogger("log/authLogger")
 
 
 shopping_list: List['str'] = list()
 date = datetime.now()
 
+
 def clear_screen():
     os.system("cls" if os.name == 'nt' else "clear")
 
 
-def write_users_info(info_file:__file__, username: str, name: str, password: str, address: str, phone: str):
+def write_users_info(info_file: __file__, username: str, name: str, password: str, address: str, phone: str): # noqa E501
     """
-    this func get users info as arg and write the infos as nested dict in json file
+    this func get users info as arg and write the infos
+    as nested dict in json file
     Parameters
     ----------
     info_file:__file__ :
@@ -45,19 +45,20 @@ def write_users_info(info_file:__file__, username: str, name: str, password: str
     -------
     """
     with open(info_file, 'r') as users_info:
-        data=json.load(users_info)
-        info_dict = {'username':f'{username}',
-        'full_name':f'{name}',
-        'password':f'{password}',
-        'address':f'{address}',
-        'phone_number':f'{phone}'}
-        data[f'{username}']=info_dict
+        data = json.load(users_info)
+        info_dict = {
+            'username': f'{username}',
+            'full_name': f'{name}',
+            'password': f'{password}',
+            'address': f'{address}',
+            'phone_number': f'{phone}'}
+        data[f'{username}'] = info_dict
         info = json.dumps(data,  indent=4)
         with open(info_file, 'w') as new_user_info:
             new_user_info.write(info)
 
 
-def check_user_password(info_file:__file__, username: str, password: str) -> bool: 
+def check_user_password(info_file: __file__, username: str, password: str) -> bool: # noqa E501
     """
     this func check's user's username and password by reading it from json file
     Parameters
@@ -74,7 +75,7 @@ def check_user_password(info_file:__file__, username: str, password: str) -> boo
     """
     logged_in = False
     with open(info_file, 'r+') as file:
-        info_dict=json.load(file)
+        info_dict = json.load(file)
         try:
             assert username in info_dict.keys()
 
@@ -90,7 +91,7 @@ def check_user_password(info_file:__file__, username: str, password: str) -> boo
     return logged_in
 
 
-def read_users_info(info_file:__file__, username: str):
+def read_users_info(info_file: __file__, username: str):
     """
     this func reads user's info and show them
     Parameters
@@ -111,7 +112,7 @@ def read_users_info(info_file:__file__, username: str):
             print(f'phone number: {info_dict[username]["phone_number"]}')
 
 
-def get_users_info(info_file:__file__, username:str) -> Tuple:
+def get_users_info(info_file: __file__, username: str) -> Tuple:
     """
     this func reads user's info and return them in tuple
     Parameters
@@ -134,9 +135,10 @@ def get_users_info(info_file:__file__, username:str) -> Tuple:
     return username, password, full_name, address, phone_number
 
 
-def edit_user_info(info_file:__file__, username: str, name: str, password:str, address:str, phone:str):
+def edit_user_info(info_file: __file__, username: str, name: str, password: str, address: str, phone: str): # noqa E501
     """
-    this func read's user's info, delete the dicts and replace new dict by new info
+    this func read's user's info, delete the dicts
+    and replace new dict by new info
     Parameters
     ----------
     info_file:__file__ :
@@ -157,12 +159,13 @@ def edit_user_info(info_file:__file__, username: str, name: str, password:str, a
     with open(info_file, 'r+') as users_info:
         data = json.load(users_info)
         del data[username]
-        info_dict = {'username':f'{username}',
-        'full_name':f'{name}',
-        'password':f'{password}',
-        'address':f'{address}',
-        'phone_number':f'{phone}'}
-        data[f'{username}']=info_dict
+        info_dict = {
+            'username': f'{username}',
+            'full_name': f'{name}',
+            'password': f'{password}',
+            'address': f'{address}',
+            'phone_number': f'{phone}'}
+        data[f'{username}'] = info_dict
         info = json.dumps(data,  indent=4)
         with open(info_file, 'w') as new_user_info:
             new_user_info.write(info)
@@ -187,7 +190,6 @@ def error_messages(func):
     return add_star
 
 
-
 def add_items(shopping_list: List['str'], item: str) -> List:
     """this function add new items to our list
     Parameters
@@ -203,9 +205,10 @@ def add_items(shopping_list: List['str'], item: str) -> List:
     shopping_list.append(item)
 
 
-def get_categories(info_file:__file__, category:str):
+def get_categories(info_file: __file__, category: str):
     """
-    this func reads json file containing products info and check product's category
+    this func reads json file containing products info
+    and check product's category
     which are dict's key
     Parameters
     ----------
@@ -217,13 +220,13 @@ def get_categories(info_file:__file__, category:str):
     -------
     """
     with open(info_file, 'r+') as file:
-        info_dict=json.load(file)
+        info_dict = json.load(file)
         if category in info_dict.keys():
             for key, value in info_dict[category].items():
                 print('{}  :  {}$'.format(key, value))
 
 
-def check_available_products(info_file:__file__) -> List:
+def check_available_products(info_file: __file__) -> List:
     """
     this func check if a product is available in our store
     by checking key inner nested dict written in json file
@@ -235,8 +238,8 @@ def check_available_products(info_file:__file__) -> List:
     -------
     """
     with open(info_file, 'r+') as file:
-        info_dict=json.load(file)
-        products=list()
+        info_dict = json.load(file)
+        products = list()
         for key, value in info_dict['VEGETABLES'].items():
             products.append(key)
         for key, value in info_dict['FRUITS'].items():
@@ -246,7 +249,7 @@ def check_available_products(info_file:__file__) -> List:
     return products
 
 
-def get_products_dict(info_file:__file__) -> Dict:
+def get_products_dict(info_file: __file__) -> Dict:
     """
     this func unpack all dicts about products info from json file into one
     Parameters
@@ -257,8 +260,8 @@ def get_products_dict(info_file:__file__) -> Dict:
     -------
     """
     with open(info_file, 'r+') as file:
-        info_dict=json.load(file)
-        products_info = {**info_dict['VEGETABLES'], **info_dict['FRUITS'], **info_dict['SUPERMARKET']}
+        info_dict = json.load(file)
+        products_info = {**info_dict['VEGETABLES'], **info_dict['FRUITS'], **info_dict['SUPERMARKET']} # noqa E501
     return products_info
 
 
@@ -329,83 +332,83 @@ def get_discount_code(discount_codes: List) -> str:
     else:
         print("your bad! you didn't get any discount code this time")
 
+
 clear_screen()
 while True:
-    registration_mode=input('have you registered in our site?:[YES/NO] ').upper()
+    registration_mode = input('have you registered in our site?:[YES/NO] ').upper() # noqa E501
     clear_screen()
     match registration_mode:
         case "NO":
             print('please fill in your information to create an account')
-            username=input(msg.enter_username)
-            full_name=input(msg.enter_full_name)
-            address=input(msg.enter_address)
+            username = input(msg.enter_username)
+            full_name = input(msg.enter_full_name)
+            address = input(msg.enter_address)
             while True:
-                password=input(msg.enter_password)
-                password_confirm=input(msg.enter_pass_confirm)
-                if password==password_confirm:
+                password = input(msg.enter_password)
+                password_confirm = input(msg.enter_pass_confirm)
+                if password == password_confirm:
                     break
                 else:
                     print(msg.confirm_failed)
                     continue
-            phone_number=input(msg.enter_phone)
-            write_users_info('user_info.json', username, full_name, password, address, phone_number)
+            phone_number = input(msg.enter_phone)
+            write_users_info('user_info.json', username, full_name, password, address, phone_number) # noqa E501
             logger.info(f'{username} registered')
             break
         case "YES":
-            username=input(msg.enter_username)
-            password=input(msg.enter_password)
-            logged_in=check_user_password('user_info.json', username, password)
+            username = input(msg.enter_username)
+            password = input(msg.enter_password)
+            logged_in = check_user_password('user_info.json', username, password) # noqa E501
             if logged_in is True:
                 logger.info(f'{username} logged in')
                 break
         case _:
             print(msg.invalid_command)
             continue
-            
 
 clear_screen()
 while True:
     msg.user_bar_help()
-    user_command=input(msg.chose_command)
+    user_command = input(msg.chose_command)
     match user_command:
         case "1":
             read_users_info("user_info.json", username)
-            to_edit_info=input('if you want to edit info press 1: ')
-            if to_edit_info=='1':
+            to_edit_info = input('if you want to edit info press 1: ')
+            if to_edit_info == '1':
                 while True:
                     clear_screen()
                     msg.edit_info_help()
-                    info_edit_type=input(msg.chose_command)
+                    info_edit_type = input(msg.chose_command)
                     match info_edit_type:
                         case "1":
-                            new_username=input(msg.enter_n_username)
-                            username,password, full_name, address, phone_number=get_users_info('user_info.json', username)
-                            username=new_username
+                            new_username = input(msg.enter_n_username)
+                            username, password, full_name, address, phone_number = get_users_info('user_info.json', username) # noqa E501
+                            username = new_username
                         case "2":
                             while True:
-                                new_password=input(msg.enter_n_password)
-                                new_password_confirm=input(msg.enter_n_pass_confirm)
-                                if new_password==new_password_confirm:
-                                    username, password, full_name, address, phone_number=get_users_info('user_info.json', username)
-                                    password=new_password
+                                new_password = input(msg.enter_n_password)
+                                new_password_confirm = input(msg.enter_n_pass_confirm) # noqa E501
+                                if new_password == new_password_confirm:
+                                    username, password, full_name, address, phone_number = get_users_info('user_info.json', username) # noqa E501
+                                    password = new_password
                                     break
                                 else:
                                     continue
                         case "3":
-                            new_address=input(msg.enter_n_address)
-                            username, password, full_name, address, phone_number=get_users_info('user_info.json', username)
-                            address=new_address
+                            new_address = input(msg.enter_n_address)
+                            username, password, full_name, address, phone_number = get_users_info('user_info.json', username) # noqa E501
+                            address = new_address
                         case "4":
-                            new_phone_number=input(msg.enter_n_phone)
-                            username, password, full_name, address, phone_number=get_users_info('user_info.json', username)
-                            phone_number=new_phone_number
+                            new_phone_number = input(msg.enter_n_phone)
+                            username, password, full_name, address, phone_number = get_users_info('user_info.json', username) # noqa E501
+                            phone_number = new_phone_number
                         case "5":
                             break
                         case _:
                             clear_screen()
                             print(msg.invalid_command)
                             msg.edit_info_help()
-                    edit_user_info("user_info.json", username, full_name, password, address, phone_number)
+                    edit_user_info("user_info.json", username, full_name, password, address, phone_number) # noqa E501
             else:
                 continue
         case "2":
@@ -423,17 +426,17 @@ while True:
 clear_screen()
 msg.show_help()
 while True:
-    new_item=input(msg.add_item).upper().strip()
+    new_item = input(msg.add_item).upper().strip()
     clear_screen()
     if new_item in shopping_list:
         print(msg.exist_message())
-    else:    
+    else:
         match new_item:
             case "SEARCH":
-                to_search_item=input(msg.item_search).upper()
+                to_search_item = input(msg.item_search).upper()
                 shopping_list_search(shopping_list, to_search_item)
             case "DELETE":
-                to_delete_item=input(msg.item_delete).upper()
+                to_delete_item = input(msg.item_delete).upper()
                 delete_item(shopping_list, to_delete_item)
             case "SHOW":
                 show_list(shopping_list, new_item)
@@ -442,10 +445,10 @@ while True:
             case "HELP":
                 msg.show_help()
             case "PRODUCTS":
-                category=input(msg.product_category).upper()
+                category = input(msg.product_category).upper()
                 get_categories('shopping_list_data.json', category)
             case _:
-                available_products=check_available_products('shopping_list_data.json')
+                available_products = check_available_products('shopping_list_data.json') # noqa E501
                 if new_item in available_products:
                     add_items(shopping_list, new_item)
                 else:
@@ -453,9 +456,9 @@ while True:
 
 costs = list()
 for length in range(len(shopping_list)):
-    quantity = int(input(f'how many boxes of {shopping_list[length]} you want?: '))
+    quantity = int(input(f'how many boxes of {shopping_list[length]} you want?: ')) # noqa E501
     clear_screen()
-    available_products_dict=get_products_dict('shopping_list_data.json')
+    available_products_dict = get_products_dict('shopping_list_data.json')
     value_of_item = int(available_products_dict[shopping_list[length]])
     item_cost = (quantity)*(value_of_item)
     costs.append(item_cost)
@@ -517,4 +520,3 @@ if delivery == '1':
 if delivery == '2':
     print('we will deliver this shopping for following info')
     read_users_info('user_info.json', username)
-
